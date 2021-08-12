facd40c31331af4c1cfce558f32b82083db06e52
Apoorva Arya <apoorva20032@iiitd.ac.in>
2021-08-12 20:24:03 +0530

    typo

--- Simple-Assembler/Main.py
+++ Simple-Assembler/Main.py
@@ -155,7 +155,7 @@ def TypeF(line):
     if array[0] == 'hlt':
         encoding += Instruction[array[0]]['opcode']
         encoding += '0'*11
-        AnswerList.append(encoding)
+        Answerlist.append(encoding)
 
 #consider this the main function
 
