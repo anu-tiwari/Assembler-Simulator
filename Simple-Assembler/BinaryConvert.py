f6069b2cf55400b3501a35e65ddbb0b2ae88fb16
sausy-saus <88702651+sausy-saus@users.noreply.github.com>
2021-08-14 20:36:20 +0530

    ToBinary.py

--- Simple-Assembler/BinaryConvert.py
+++ Simple-Assembler/BinaryConvert.py
@@ -2,6 +2,7 @@ def ToBinary(decimal):
     # argument must be a decimal number in string datatype
     # returns the binary string of 8 bits (full of leading zeroes if required)
     # returns error if the number requires more than 8 bits or is negative
+    
     decimal = int(decimal)
     if decimal>=256 or decimal<0:
         return 'overflow error'
